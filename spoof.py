ead811d0dd0dbcfada633948ee01512b169a2703
ethan <ethanlee012345@gmail.com>
2023-03-16 20:36:00 -0500

    README update, reading date wrong fixed

--- spoof.py
+++ spoof.py
@@ -18,11 +18,11 @@ def progress_bar(current, total, bar_length=20):
     print(f'Progress: [{arrow}{padding}] {int(fraction*100)}%', end=ending)
 
 
-parser = argparse.ArgumentParser(description="A program to spoof commits to GitHub so that potential employers won't see that you're lazy and don't actually code lol\nOnly tested on Windows idk if it works on Linux or anything else")
+parser = argparse.ArgumentParser(description="A program to spoof commits to GitHub so that potential employers won't see that you're lazy and don't actually code lol. Only tested on Windows idk if it works on Linux or anything else")
 parser.add_argument("-rp", "--repo_path", default=None, nargs=1, metavar="path", type=str, help="The path of the repo you want to spoof commit dates to")
 parser.add_argument("-sd", "--start_date", default=None, nargs=1, metavar="short git date", type=str, help="The date to start spoofing commits to in Git's 'short' date format (YYYY-MM-DD)")
 parser.add_argument("-ed", "--end_date", default=None, nargs=1, metavar="short git date", type=str, help="The date to stop spoofing commits to in Git's 'short' date format (YYYY-MM-DD)")
-parser.add_argument("--blatant", default=False, nargs=1, metavar="boolean", type=bool, help="Whether you want the commits to look blatant and suspicious or natural and not sus")
+parser.add_argument("--blatant", default="false", nargs=1, metavar="boolean", type=str, help="Commit every single day")
 
 
 args = parser.parse_args()
@@ -30,7 +30,7 @@ path = args.repo_path[0]
 
 git_start_date = args.start_date[0]
 git_end_date = args.end_date[0]
-blatant = args.blatant[0]
+blatant = args.blatant[0].lower() == "true"
 
 try:
     if ( len(git_end_date) != 10 or len(git_start_date) != 10 ):
@@ -38,15 +38,14 @@ try:
     # regex for correctly formatted date string 
     if ( re.search( "[1-2][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]", git_start_date ) == None or re.search( "[1-2][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]", git_end_date ) == None ):
         raise Exception("Bad Date Format")
-    
-    start_date = datetime.date(int(git_start_date[0 : 4]), int(git_start_date[5 : 7]), int(git_start_date[9 : 11]))
-    end_date = datetime.date(int(git_end_date[0 : 4]), int(git_end_date[5 : 7]), int(git_end_date[9 : 11]))
+
+    start_date = datetime.date(int(git_start_date[0 : 4]), int(git_start_date[5 : 7]), int(git_start_date[8 : 10]))
+    end_date = datetime.date(int(git_end_date[0 : 4]), int(git_end_date[5 : 7]), int(git_end_date[8 : 10]))
 except:
     print("Invalid date(s)!")
     exit()
 
 num_days = abs(start_date - end_date).days
-
 
 if not os.path.exists(args.repo_path[0]):
     print("Path does not exist!")
@@ -59,7 +58,7 @@ else:
     spoof_to.close()
     should_commit = [True, True, False]
     for i in range(num_days):
-        if ( random.choice(should_commit) ):
+        if ( random.choice(should_commit) or blatant ):
             num_commits = random.randint(1, 5)
             for j in range(num_commits):
                 spoof_to = open(args.repo_path[0] + "\\spoof", "w")
